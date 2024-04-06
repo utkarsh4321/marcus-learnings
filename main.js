749856a4b46197a6a169e67a704ba48082d91982
utkarsh srivastava <30936607+utkarsh4321@users.noreply.github.com>
2024-04-06 18:02:53 +0000

    Js first class

---
+++ main.js
@@ -0,0 +1,28 @@
+// primitive data type
+// string
+//number 
+//boolean // true false
+// set
+//null
+//undefined
+let tiger = 123; // 133
+let aa = null;
+// non pritmitive type data type
+// object
+//array
+
+
+// object
+const aaf = {
+    name:'suzuki',
+    color:'white'
+}
+console.log(aaf)
+aaf.color = 'blue'
+console.log(aaf)
+
+// Home Work for the next class
+// Just take two number and then print the sum,division,subtraction and multiplication of it.
+
+
+
