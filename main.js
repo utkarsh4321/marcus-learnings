b5f768b0314e893bc6977629758daba266f1fdb2
utkarsh srivastava <utkarshsrivastava@utkarshs-MacBook-Pro.local>
2024-04-29 23:32:08 +0530

    day-3 javascript

--- main.js
+++ main.js
@@ -5,24 +5,39 @@
 // set
 //null
 //undefined
-let tiger = 123; // 133
-let aa = null;
+// let tiger = 123; // 133
+// let aa = null;
 // non pritmitive type data type
 // object
 //array
 
 
 // object
-const aaf = {
-    name:'suzuki',
-    color:'white'
-}
-console.log(aaf)
-aaf.color = 'blue'
-console.log(aaf)
+// const aaf = {
+//     name:'suzuki',
+//     color:'white'
+// }
+// console.log(aaf)
+// aaf.color = 'blue'
+// console.log(aaf)
 
 // Home Work for the next class
 // Just take two number and then print the sum,division,subtraction and multiplication of it.
+// && --> AND gate || --> OR gate !--> Not Gate
+// FALSY VALUE IN JS
+// '' empty string , null , undefined,false,0
 
 
-
+// if('' || false){
+//     console.log('yes')
+// }else if(23 !== 23){
+//     console.log('Hello')
+// }
+// else if(0){
+//     console.log('No')
+// }
+// Home work 
+let n1= parseInt(prompt("Enter a number: "))
+let n2 = parseInt(prompt("Enter a number: "))
+let opt = prompt("Enter the operation you want perform like +,-,/,%")
+console.log(aa)
